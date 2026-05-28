0e8f16ea73adce636e81e0ca5971fd5822b49e18
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-05-28 15:04:40 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.19 ms, Memory: 31 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution
+{
+    public int solution(int a, int b, int c)
+    {
+        if(a != b && b != c && c != a)
+            return a + b + c;
+        else if(a == b && a == c)
+            return (a + b + c) * (a * a + b * b + c * c)
+                    * (a * a * a + b * b * b + c * c * c);
+        else if(a == b || b == c || c == a)
+            return (a + b + c) * (a * a + b * b + c * c);
+        
+        return 0;
+    }
+}
