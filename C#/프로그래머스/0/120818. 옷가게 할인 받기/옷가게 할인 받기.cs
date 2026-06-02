80d4a8c01c257ca2d3b66cc2bcc8389a3424992d
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-02 12:19:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.15 ms, Memory: 30.9 MB -BaekjoonHub

--- C#/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
+++ C#/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
@@ -5,6 +5,7 @@ public class Solution
     public int solution(int price)
     {
         // 10만원 이상 -> 5% | 30만원 이상 -> 10% | 50만원 이상 -> 20%
+        /*
         float sale = price;
         
         if(price >= 500000)
@@ -17,5 +18,10 @@ public class Solution
             sale = 0f;
         
         return (int)MathF.Floor(price - sale);
+        */
+        
+        return price >= 500000 ? (int)(price * 0.8f) :
+               price >= 300000 ? (int)(price * 0.9f) :
+               price >= 100000 ? (int)(price * 0.95f) : price;
     }
 }
