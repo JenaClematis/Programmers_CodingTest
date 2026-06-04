1c16508cc48d08f25b492f21ec60641bfe02e1b0
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-04 11:30:55 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.13 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(int[] num_list)
+    {
+        int[] answer = new int[2];
+        
+        foreach(int num in num_list)
+            answer[num % 2]++;
+        
+        return answer;
+    }
+}
