62a8856d954c351e695be7e093636c5adc7a941f
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-04 11:17:40 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 9.15 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Solution
+{
+    public string solution(string my_string, int[,] queries)
+    {
+        /*
+        for(int i = 0; i < queries.GetLength(0); i++)
+        {
+            for(int j = 0; j < queries.GetLength(1); j++)
+                Console.Write($"[ {i}, {j} ] = {queries[i, j]} ");
+            
+            Console.WriteLine();
+        }
+        */
+        
+        string answer = my_string;
+        char[] target;
+        int start, end;
+        
+        for(int i = 0; i < queries.GetLength(0); i++)
+        {
+            start = queries[i, 0];
+            end = queries[i, 1] - start + 1;
+            
+            target = answer.Substring(start, end).ToCharArray();
+            Array.Reverse(target);
+            
+            answer = answer.Remove(start, end).Insert(start, string.Join("", target));
+        }
+        
+        return answer;
+    }
+}
