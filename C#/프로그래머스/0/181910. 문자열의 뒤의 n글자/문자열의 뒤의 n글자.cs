ed9ecd90dad478b1c8a7f7c59e8f5aa94502361d
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-05 09:53:11 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.15 ms, Memory: 30.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public string solution(string my_string, int n)
+    {
+        //Console.WriteLine(my_string.Length);
+        
+        return my_string.Substring(my_string.Length - n, n);
+    }
+}
