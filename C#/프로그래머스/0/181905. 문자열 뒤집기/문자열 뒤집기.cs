36c89a046dfdee698f47b9c492fb169bc433c0b5
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-08 16:29:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 3.12 ms, Memory: 30.7 MB -BaekjoonHub

--- C#/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
+++ C#/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
@@ -5,6 +5,7 @@ public class Solution
 {
     public string solution(string my_string, int s, int e)
     {
+        // 그렇네, my_string을 char[]로 바꾼 다음에 그냥 해당 글자를 reverse하고 string으로 바꿀 생각을 못했지..?
         string target = my_string.Substring(s, e - s + 1);
         string reverse = string.Join("", target.Reverse().ToArray());
         
