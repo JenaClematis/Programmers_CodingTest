aeae6e52766f1d55eea84359b2e0ddb20619434f
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-08 09:49:26 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 2.90 ms, Memory: 31.2 MB -BaekjoonHub

--- C#/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cs
+++ C#/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cs
@@ -4,6 +4,7 @@ public class Solution
 {
     public int solution(string my_string, string is_prefix)
     {
+        // IndexOf로도 풀 수 있구나
         return my_string.StartsWith(is_prefix) ? 1 : 0;
     }
 }
