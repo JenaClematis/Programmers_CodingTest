3c2a030cfc560954807f6d4912f9b63abe125954
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-09 10:03:48 +0900

    [level 0] Title: qr code, Time: 0.59 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/0/181903. qr code/qr code.cs
+++ C#/프로그래머스/0/181903. qr code/qr code.cs
@@ -6,6 +6,7 @@ public class Solution
     {
         string answer = "";
         
+        // 아; Linq 공부 열심히 했으면서 Linq 쓸 생각은 왜 안나냐....
         for(int i = 0; i < code.Length; i++)
             answer += i % q == r ? code[i].ToString() : "";
         
