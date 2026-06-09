ed9a249e0b1dcbe24db9fad053902b025e17bd4e
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-09 11:36:22 +0900

    [level 0] Title: 카운트 다운, Time: 0.73 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int start_num, int end_num)
+    {
+        return Enumerable.Range(end_num, start_num - end_num + 1).Reverse().ToArray();
+    }
+}
