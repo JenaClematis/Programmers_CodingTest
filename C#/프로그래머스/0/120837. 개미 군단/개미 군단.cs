6e198e2ac0e0e7fe05018cf3cd94ca18caedddd4
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-10 15:42:16 +0900

    [level 0] Title: 개미 군단, Time: 0.17 ms, Memory: 30.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
+++ C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -7,6 +7,7 @@ public class Solution
         int curHp = hp;
         int answer = 0;
         
+        // 아 맞네; 그냥 각각의 개미의 수를 구했어도 되네... 왜 한꺼번에 구하려고 했지...
         while(curHp > 0)
         {
             answer += curHp / 5 > 0 ? curHp / 5 : curHp / 3 > 0 ? curHp / 3 : curHp / 1;
