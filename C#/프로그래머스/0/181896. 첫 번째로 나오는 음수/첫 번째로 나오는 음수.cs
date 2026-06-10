65486861fce1055c3109ed0c2323120a99de6119
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-06-10 12:54:34 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.15 ms, Memory: 30.7 MB -BaekjoonHub

--- C#/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
+++ C#/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
@@ -4,6 +4,7 @@ public class Solution
 {
     public int solution(int[] num_list)
     {
+        // Array.FindIndex가 있구나?
         for(int i = 0; i < num_list.Length; i++)
             if(num_list[i] < 0)
                 return i;
