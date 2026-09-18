758e1789970a91fcb79732a0c643fe36fe48faaa
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-09-18 21:56:41 +0900

    [level 0] Title: 5명씩, Time: 0.15 ms, Memory: 46.3 MB -BaekjoonHub

--- C#/프로그래머스/0/181886. 5명씩/5명씩.cs
+++ C#/프로그래머스/0/181886. 5명씩/5명씩.cs
@@ -5,6 +5,7 @@ public class Solution
     public string[] solution(string[] names)
     {
         // 정수 나눗셈에 올림 효과 추가 방법 : N + 나누는 수 - 1
+        // 5의 배수인가 아닌가로 나눠서 /5 : /5 + 1 하는 방법도 있었음
         int length = (names.Length + 4) / 5;
         string[] answer = new string[length];
         
