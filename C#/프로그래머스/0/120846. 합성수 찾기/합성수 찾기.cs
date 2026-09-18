026fc075ed235591f8280bc98050e6519236b586
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-09-19 00:08:27 +0900

    [level 0] Title: 합성수 찾기 -BaekjoonHub

--- C#/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cs
+++ C#/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cs
@@ -6,10 +6,21 @@ public class Solution
     {
         int answer = 0;
         
-        for(int i = 2; i < n + 1; i++)
-            if((i % 2 == 0 && i != 2) || (i % 3 == 0 && i != 3)
+        // 약수는 쌍으로 존재하기 때문에 제곱을 통해 확인 범위를 제한할 수 있음
+        for(int i = 4; i < n + 1; i++)
+            for(int j = 2; j * j < i + 1; j++)
+                if(i % j == 0)
+                {
+                    answer++;
+                    break;
+                }
+        
+        /*
+        for(int i = 4; i < n + 1; i++)
+            if(i % 2 == 0 || i % 3 == 0
                 || (i % 5 == 0 && i != 5) || (i % 7 == 0 && i != 7))
                 answer++;
+        */
         
         return answer;
     }
