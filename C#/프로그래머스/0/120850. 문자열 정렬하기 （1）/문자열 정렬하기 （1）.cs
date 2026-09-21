bc81fd6209f6c11098b17c721b417025e051a000
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-09-21 21:28:36 +0900

    [level 0] Title: 문자열 정렬하기 (1) -BaekjoonHub

--- C#/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cs
+++ C#/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class Solution
 {
     public int[] solution(string my_string)
     {
+        /*
         List<int> answer = new List<int>();
         
         for(int i = 0; i < my_string.Length; i++)
@@ -12,6 +14,20 @@ public class Solution
                 answer.Add(num);
         
         answer.Sort();
+        
+        return answer.ToArray();
+        */
+        
+        List<int> answer = new List<int>();
+        MatchCollection matches = Regex.Matches(my_string, "[0-9]");
+        
+        if(matches.Count > 0)
+        {
+            for(int i = 0; i < matches.Count; i++)
+                answer.Add(Convert.ToInt32(matches[i].ToString()));
+            
+            answer.Sort();
+        }
         
         return answer.ToArray();
     }
