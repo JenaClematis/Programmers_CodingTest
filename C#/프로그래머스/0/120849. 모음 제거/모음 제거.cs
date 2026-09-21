628e53fcc1ca5d96d030e84767b28379d1f29dab
JenaClematis <74389349+JenaClematis@users.noreply.github.com>
2026-09-21 20:41:27 +0900

    [level 0] Title: 모음 제거, Time: 7.41 ms, Memory: 46.8 MB -BaekjoonHub

--- C#/프로그래머스/0/120849. 모음 제거/모음 제거.cs
+++ C#/프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Text.RegularExpressions;
 
 public class Solution
 {
     public string solution(string my_string)
     {
         string answer = my_string;
-        return answer.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
+        //return answer.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
+        return Regex.Replace(answer, "[a,e,i,o,u]", "");
     }
 }
